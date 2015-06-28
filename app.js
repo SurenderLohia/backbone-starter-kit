26368136e4c44311bfe64c336abf98e669db26f8
SurenderLohia <surender@fantain.com>
2015-06-28 17:33:26 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-require(['libs/text!header.html', 'libs/text!home.html', 'libs/text!footer.html'], function (headerTpl, homeTpl, footerTpl) {
+require(['libs/text!header.html', 'libs/text!home.html', 'libs/text!footer.html', 'libs/text!about.html', 'libs/text!contact.html'], function (headerTpl, homeTpl, footerTpl, aboutTpl, contactTpl) {
+
+	var skills = [
+		'HTML',
+		'CSS',
+		'JavaScript',
+		'Knockout',
+		'Angular',
+		'Backbone'
+	]
 	
 	var ApplicationRouter = Backbone.Router.extend({
 		routes: {
 			"": "home",
-			"*actions": "home"
+			"about": 'about',
+			'contact': 'contact'
 		},
 		initialize: function() {
 			this.headerView = new HeaderView();
@@ -12,8 +22,20 @@ require(['libs/text!header.html', 'libs/text!home.html', 'libs/text!footer.html'
 			this.footerView.render();
 		},
 		home: function() {
-			this.homeView = new HomeView();
+	        var model = {};
+	        model.skills = skills;
+
+			this.homeView = new HomeView(model);
 			this.homeView.render();
+		},
+		about: function() {
+			this.aboutView = new AboutView();
+			this.aboutView.render();
+		},
+
+		contact: function() {
+			this.contactView = new ContactView();
+			this.contactView.render();
 		}
 	});
 
@@ -33,25 +55,68 @@ require(['libs/text!header.html', 'libs/text!home.html', 'libs/text!footer.html'
 	FooterView = Backbone.View.extend({
 		el: "#footer",
 		template: footerTpl,
+
+		events: {
+			'click .about-us-link': 'goToAbout',
+			'click .contact-link': 'goToContact',
+			'click .home-link': 'goToHome'
+		},
+
 		render: function() {
 			this.$el.html(_.template(this.template));
+		},
+
+		goToAbout: function(e) {
+			e.preventDefault();
+			app.navigate('about', true);
+		},
+
+		goToContact: function(e) {
+			e.preventDefault();
+			app.navigate('contact', true);
+		},
+
+		goToHome: function(e) {
+			e.preventDefault();
+			app.navigate('', true);
 		}
-	})
+	});
+
 	HomeView = Backbone.View.extend({
 		el: "#content",
 		// template: "home.html",
-		template: homeTpl,
-		initialize: function() {
-
+		template: _.template(homeTpl),
+		initialize: function(options) {
+			this.model = options;
 		},
+
+		render: function() {
+			$(this.el).html(this.template(this.model));
+		},
+	});
+
+	AboutView = Backbone.View.extend({
+		el: '#content',
+		template: aboutTpl,
 		render: function() {
 			$(this.el).html(_.template(this.template));
 		}
 	});
-	
+
+	ContactView = Backbone.View.extend({
+		el: '#content',
+		template: contactTpl,
+		render: function() {
+			$(this.el).html(_.template(this.template));
+		}
+	});
+
 	
 	app = new ApplicationRouter();
-	Backbone.history.start();	
+
+	Backbone.history.start({
+		pushState: true		
+	});
 });
 
 
